8dee9a68a62fccdc5e906e36c7a98b2459369ecf
pablobgldo <pablobgldo@gmail.com>
2024-01-12 14:14:55 +0000

    Added descriptions for different functionalities

--- src/routes.py
+++ src/routes.py
@@ -1,38 +1,39 @@
 from flask import Flask, request, jsonify
 from utils import process_csv, infer_data_types, detect_outliers
-from db import get_conn, create_table, insert_data
+from db import get_conn, create_table, insert_data, close_conn
 
 app = Flask(__name__)
 
 @app.route('/upload-csv', methods=['POST'])
 def upload_csv():
+    # Returns a 400 message if no file part in request.
     if 'file' not in request.files:
-        return jsonify({'error': 'No file part'}), 400
+        return jsonify({'Error': 'No file part'}), 400
 
     file = request.files['file']
 
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'}), 400
-
-    if file and file.filename.endswith('.csv'):
+    if file.filename.endswith('.csv'):
+        # Reads CSV file and converts it into DataFrame.
         df = process_csv(file)
+        # Infers SQL data types from DataFrame.
         dtypes = infer_data_types(df)
+        # Detects outliers in DataFrame using Z-score and adds Z-score column to Dataframe.
         outliers = detect_outliers(df)
+        # Removes Z-score column from Dataframe as it is no longer necessary.
         del df['z_score']
         
+        # Creates table (if it does not exist) and inserts data into it.
+        # Returns 200 if data is uploaded properly (no outliers) and 400 if outliers are found. 
         if outliers.empty:
-            try:
-                conn = get_conn()
-                create_table(conn, 'hello', dtypes)
-                insert_data(conn, 'hello', df)
-                print('Data has now been inserted as there are no outliers')
-            except Exception as e:
-                print('Data could not be inserted despite lack of outliers: {e}')
+            conn = get_conn()
+            create_table(conn, 'test', dtypes)
+            insert_data(conn, 'test', df)
+            close_conn(conn)
+            return jsonify({'Message': 'File successfully uploaded'}), 200
         else:
-            print('We have detected outliers so data will not be inserted')
-
-
-        return jsonify({'message': 'File successfully uploaded'}), 200
+            return jsonify({'Message': 'File could not be uploaded due to outliers'}), 400 
+ 
     else:
-        return jsonify({'error': 'Invalid file format'}), 400
+        # Returns a 400 message if a file with a format other than .CSV is uploaded.
+        return jsonify({'Error': 'Invalid file format'}), 400
     
--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,18 @@
 import pandas as pd
 
+# Reads CSV file and converts it into DataFrame.
 def process_csv(file_path):
-    """
-    Reads CSV file and converts it into a DataFrame.
-    """
     try:
         df = pd.read_csv(file_path)
-         # Convert 'timestamp' column to datetime. Not dynamic as code will only work if there's a column called 'timestamp'.
+        # Converts 'timestamp' column in Dataframe to datetime object. 
+        # Not the most dynamic as code only works if there's a column called 'timestamp'.
         df['timestamp'] = pd.to_datetime(df['timestamp'])
         return df
     except Exception as e:
         raise ValueError(f"Error processing CSV file: {e}")
-    
+
+# Takes DataFrame and returns dictionary with 'column name' as key and 'SQL data type' as value.
 def infer_data_types(df):
-    """
-    Infers SQL data types from a DataFrame.
-    """
     dtypes = {}
     for column in df.columns:
         if pd.api.types.is_integer_dtype(df[column]):
@@ -28,13 +25,15 @@ def infer_data_types(df):
             dtypes[column] = "VARCHAR"
     return dtypes
 
+# Detects outliers in a DataFrame using Z-score method. 
+# Returns empty Dataframe if no outliers found.
 def detect_outliers(df, threshold=2.58):
-    """
-    Detects outliers in a DataFrame using the Z-score method.
-    Attempted a threshold of 3 but didn't find outlier with value 100 so lowered it to 2.58.
-    """
+    # Initially attempted a threshold of 3 but did not find outlier value of 100 so lowered it to 2.58.
     outliers = pd.DataFrame()
-    for column in df.select_dtypes(include=[ 'float64', 'int64']):
+    # Performs outlier detection only on columns with numerical values.
+    for column in df.select_dtypes(include=['number']):
+        # Calculates Z-score for each value in column.
         df['z_score'] = (df[column] - df[column].mean()) / df[column].std()
+        # Appends Z-score to 'outliers' if Z-score is higher than threshold.
         outliers = outliers._append(df[df['z_score'].abs() > threshold])
-    return outliers.drop(columns=['z_score'])
+    return outliers
